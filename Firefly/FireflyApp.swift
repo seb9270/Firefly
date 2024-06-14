a44a6ca1c79be737f5610a74f6f141c153f41e39
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-06-14 09:52:28 +0100

    Initial Commit

---
+++ Firefly/FireflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireflyApp.swift
+//  Firefly
+//
+//  Created by Micko, Sebastian (AMM) on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FireflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
